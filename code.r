390138040be5b820c6ff38a274fd2accc745a475
monty <muntazir02@gmail.com>
2019-12-08 14:03:09 +0500

    comprehensive list of packages explored during the porgram

---
+++ code.r
@@ -0,0 +1,27 @@
+pacman::p_load(
+  tidyverse, #'tidyverse' is a set of packages. Ggplot2,dplyr, tidyr, readr, purrr, tibble, stringr, forcats
+  gtools, #Functions to assist in R programming, eg logit transformation, permutations & combs etc.
+  pacman, #Tools to more conveniently perform tasks associated with add-on packages
+  #dplyr, #grammar of data manipulation. Eg mutate, filter, select, summarise,arrange
+  GGally, # 'GGally' extends 'ggplot2' by adding several functions 
+  #ggplot2, #ggplot2 is a data visualization package
+  ggthemes, #Some extra themes, geoms, and scales for 'ggplot2'
+  ggvis, #ggvis is a data visualization package
+  ggrepel, #Labels repel away from each other and away from the data points
+  gganimate, #transitions and animations in a flexible and extensible way.
+  scales, #Graphical scales map data to aesthetics
+  gridextra, #user-level functions to work with "grid" graphics, notably to arrange multiple grid-based plots on a page, and draw tables.
+  gifski, #Multi-threaded GIF encoder 
+  png, #simple way to read, write and display bitmap images
+  hrbrthemes, #extra 'ggplot2' themes, scales and utilities
+  httr, #Useful tools for working with HTTP organised by HTTP verbs (GET(), POST(), etc).
+  lubridate, #makes it easier to work with dates and times
+  plotly, #Create interactive web graphics from 'ggplot2' graphs
+  rio, #Streamlined data import and export
+  rmarkdown, #Convert R Markdown documents into a variety of formats.
+  shiny, #Makes it incredibly easy to build interactive web applications with R
+  #stringr, #Simple, Consistent Wrappers for Common String Operations
+  #tidyr, #Tools to help to create tidy data
+  knitr, #dynamic report generation & formatting in R
+  dslabs #Datasets and functions that can be used for data analysis practice
+) 
